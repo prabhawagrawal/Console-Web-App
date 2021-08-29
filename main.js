3185cdfb13935b8a9e9f981e9529e4e0cee6d5a9
prabhawagrawal <77531005+prabhawagrawal@users.noreply.github.com>
2021-08-29 21:35:31 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+var fst_name = "Prabhaw";
+var lst_name = " Agrawal";
+var full_name = fst_name.concat(lst_name);
+console.log("Hi, My name is " + full_name);
+
+var a = 81;
+var b = 90;
+
+console.log("The Square Root of 4 is " + Math.sqrt(a));
+
+console.log("81 + 90 is " + (81 + 90));
+
+console.log("90 - 81 is " + (90-81));
+
+console.log("90 * 81 is " + (90*81));
+
+console.log("90 / 81 is " + Math.round(90/81));
